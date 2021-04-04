89c5e2e874dc0c2f24afd865c23155641bfc0f5e
jlsantia <jlsantia@ucsc.edu>
2021-04-03 18:58:13 -0700

    Finished part3 of Lab0. Successfully generated tone with speaker and amp pcb.

---
+++ Lab0/Lab0.X/part3.c
@@ -0,0 +1,31 @@
+/* 
+ * File:   part3.c
+ * Author: Jose
+ *
+ * Created on April 3, 2021, 12:13 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "BOARD.h"
+#include "ToneGeneration.h"
+
+/*
+ * 
+ */
+int main(void)
+{
+    BOARD_Init(); //init BOARD
+    ToneGeneration_Init(); //init ToneGeneration
+    
+    ToneGeneration_SetFrequency(TONE_293); //set ToneGeneration frequency
+    ToneGeneration_ToneOn(); //turn ToneGeneration on
+    
+    while(1){ //infinite loop
+        //keep tone on
+    }  
+    
+    while(1); //if embedded we never exit
+}
+
